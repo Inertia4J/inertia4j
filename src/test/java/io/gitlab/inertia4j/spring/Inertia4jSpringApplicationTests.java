df28a8dc7dbf4b1711baf435b2bc615879ee475d
Eduardo Fonseca <edrd.fons@gmail.com>
2025-02-27 20:30:06 -0300

    Bootstrap project

---
+++ src/test/java/io/gitlab/inertia4j/spring/Inertia4jSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitlab.inertia4j.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inertia4jSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
