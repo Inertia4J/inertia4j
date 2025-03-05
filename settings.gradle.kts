23127b127fb15e335aa9a4fa03bd183d6023f3af
Eduardo Fonseca <edrd.fons@gmail.com>
2025-03-05 20:20:25 -0300

    Change Gradle project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "inertia4j-spring"
+rootProject.name = "inertia4j"
 
 include("inertia4j.core")
 include("inertia4j.jackson")
