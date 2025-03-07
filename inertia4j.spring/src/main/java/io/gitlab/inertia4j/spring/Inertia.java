3bcac8819e5a41b07af27a3e48f6af5b79858909
Pedro <pfronchetti@gmail.com>
2025-03-06 23:17:36 -0300

    Docs added on Inertia facade methods

--- inertia4j.spring/src/main/java/io/gitlab/inertia4j/spring/Inertia.java
+++ inertia4j.spring/src/main/java/io/gitlab/inertia4j/spring/Inertia.java
@@ -13,14 +13,35 @@ public class Inertia {
         "templates/app.html"
     );
 
+    /*
+     * Calls the render method for the current InertiaSpringRenderer instance.
+     *
+     * @params component name of the component to render in the client
+     * @param props regular response data
+     */
     public static ResponseEntity<String> render(String component, Object props) {
         return renderer.render(component, props);
     }
 
+    /*
+     * Calls the render method for the current InertiaSpringRenderer instance.
+     *
+     * @param url value of the URL field in response
+     * @params component name of the component to render in the client
+     * @param props regular response data
+     */
     public static ResponseEntity<String> render(String url, String component, Object props) {
         return renderer.render(url, component, props);
     }
 
+    /*
+     * Calls the render method for the current InertiaSpringRenderer instance.
+     *
+     * @param WebRequest HTTP request
+     * @param url value of the URL field in response
+     * @param component name of the component to render in the client
+     * @param props regular response data
+     */
     public static ResponseEntity<String> render(
         WebRequest request,
         String url,
